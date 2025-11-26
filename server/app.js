c105128c326ef775ec0efbd72c36c47620dd476a
Harsh <gamerpandeyharsh@gmail.com>
2025-11-26 15:49:05 +0530

    Updated Backend Setup with proper Structured Code

--- server/app.js
+++ server/app.js
@@ -1,5 +1,11 @@
 import express from "express";
+import UserRouter from "./routers/user.route.js";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/users", UserRouter)
 
 export default app;
---
+++ server/config/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const ConnectToDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log(`Error Connecting to DB: ${error}`);
+  }
+};
+
+export default ConnectToDB;
